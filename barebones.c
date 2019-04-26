dcde810e5dddc23e2dc9060d673978de4ebeec95
Alec Wicklund <awicklund@umail.ucsb.edu>
2019-04-26 16:27:43 -0700

    Got barebones to link to bareTester

--- barebones.c
+++ barebones.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <setjmp.h>
 #include <unistd.h>
-
+#include <stdlib.h>
 
 
 
@@ -11,7 +11,7 @@ void pthread_init(){
 
 }
 
-int pthread_create(pthread_t *thread, const pthread_attr *attr, void* (*start_routine) (void*), void *arg){
+int pthread_create(pthread_t *thread, const pthread_attr_t *attr, void* (*start_routine) (void*), void *arg){
     return 0;
     
 }
